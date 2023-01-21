83d2bbeab9525d55ca5112020e30340be4dc5d5f
trevidia <trevidia@yahoo.com>
2023-01-21 14:41:12 +0100

    error handling

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import {withAuth} from "next-auth/middleware";
 
 export default withAuth(
     function middleware(req){
-        console.log(req)
+        // console.log(req)
     },
     {
         callbacks: {
